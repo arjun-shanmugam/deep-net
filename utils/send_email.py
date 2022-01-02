cf1e241e8fe3f61540fa8f5c77a97fdffd7e6e54
AndyDelworth <andrew_delworth@brown.edu>
2022-01-02 14:22:29 -0500

    Update send email

--- utils/send_email.py
+++ utils/send_email.py
@@ -1,4 +1,4 @@
-import smtplib, ssl
+import smtplib, ssl, datetime
 
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
@@ -6,12 +6,19 @@ sender_email = "nba.preds.by.the.big.dawgs@gmail.com"  # Enter your address
 receiver_emails = ["andrew_delworth@brown.edu"]  # Enter receiver address
 password = "bigdawgsgottaeat"
 message = """\
-Subject: Hi there
+Subject: Big Dawgs Predictions - {}
 
-This message is sent from Python."""
+Predictions\n"""
 
-for receiver_email in receiver_emails:
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
+#take list of predictions, send email to list of users
+#TODO - Predictions argument needs to be well-defined, need to
+#be able to identify which game each pred is for
+def send_email(predictions):
+    prediction_string = message
+    for pred in predictions:
+        prediction_string += pred + "\n"
+    for receiver_email in receiver_emails:
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, prediction_string.format(datetime.date.today()))
