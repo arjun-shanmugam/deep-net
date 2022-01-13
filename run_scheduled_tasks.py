0cf5fd2a4fafd9c35a436f1ac4691abf42bd479c
AndyDelworth <andrew_delworth@brown.edu>
2022-01-13 11:21:34 -0500

    delete bad database.py that i messed up

--- run_scheduled_tasks.py
+++ run_scheduled_tasks.py
@@ -1,15 +1,25 @@
 import schedule
 import time
 from utils.send_email import send_email
+from database import Database
+from git import Repo
+from utils.run_git_commands import git_push_updated_db
  
-def daily_tasks():
-    #TODO run daily tasks
-    send_email(["1"])
+def daily_tasks(db, git_repo):
+    #Fetch updated data
+    db.update_games_table()
+    db.update_boxscores_table()
+    send_email(["1"]) #TODO: Make preds, and email them
+    git_push_updated_db()
+
+  
+schedule.every(5).minutes.do(daily_tasks) #9 AM
 
 def run():
-    schedule.every().minutes.do(daily_tasks) #9 AM
+    repo = git.Repo('deep-net')
+    db = Database("../data/nbastats.db")
     while True:
-        schedule.run_pending()
+        schedule.run_pending(db, repo)
         time.sleep(1)
 
 if __name__ == "__main__":
