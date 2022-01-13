3f4589cd702aa7dbea937af1fb9a5b6dca8280a5
AndyDelworth <andrew_delworth@brown.edu>
2022-01-13 12:12:35 -0500

    Rename, push to remote

--- run_scheduled_tasks.py
+++ run_scheduled_tasks.py
@@ -1,25 +1,22 @@
 import schedule
 import time
 from utils.send_email import send_email
-from database import Database
-from git import Repo
 from utils.run_git_commands import git_push_updated_db
+from deep_net.database import Database
  
-def daily_tasks(db, git_repo):
+def daily_tasks(db):
     #Fetch updated data
     db.update_games_table()
     db.update_boxscores_table()
     send_email(["1"]) #TODO: Make preds, and email them
     git_push_updated_db()
 
-  
-schedule.every(5).minutes.do(daily_tasks) #9 AM
-
 def run():
-    repo = git.Repo('deep-net')
-    db = Database("../data/nbastats.db")
+    db = Database("data/nbastats.db")
+    schedule.every(5).minutes.do(daily_tasks(db=db)) #9 AM
+    time.sleep(120) #sleep 2 mins (testing purposes)
     while True:
-        schedule.run_pending(db, repo)
+        schedule.run_pending()
         time.sleep(1)
 
 if __name__ == "__main__":
